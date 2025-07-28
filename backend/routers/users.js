c2461d755b4c7f3e1128c79df3a6555a228bd7f9
ajaaaah <ajah14@ymail.com>
2025-07-27 21:08:07 -0400

    Created CRUD operations for user model and router

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -14,5 +14,76 @@ router.get(`/`, async (req, res) => {
     res.send(userList);
 });
 
+// Get a user by ID
+router.get('/:id', async (req,res) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    res.send(user);
+})
+
+//Create a new user
+router.post(`/`, async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        street: req.body.street,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin || false
+
+    });
+
+    user = await user.save();
+
+    if(!user){
+        return res.status(400).json({ success: false, message: 'the user could not be created' });
+    }
+
+    res.send(user);
+});
+
+//Update a user 
+router.put('/:id',async (req,res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            street: req.body.street,
+            city: req.body.city,
+            zip: req.body.zip,
+            country: req.body.country,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin
+        },
+        { new: true }
+    );
+
+    if(!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    res.send(user);
+})
+
+
+//Delete a user
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    res.send({ success: true, message: 'User deleted successfully' });
+});
 
 module.exports = router;
